952f0f8f227573d37a97c6168cd2372ea3bdcacc
jazen <stephan@frozencherry.de>
2009-04-30 17:04:09 +0200

    Added (un-)install script. Updated .gitignore and README

---
+++ install.rb
@@ -0,0 +1,15 @@
+require "fileutils"
+
+alias :L :lambda
+
+Joiner = L { |base| L { |*others| File.join(base, *others) } }
+Link   = L { |from, to| FileUtils.ln_s(from, to) rescue puts("#{to} exists.") }
+
+Cwd    = Joiner[ File.expand_path(File.dirname(__FILE__)) ]
+Home   = Joiner[ File.expand_path("~") ]
+
+Link[ Cwd['.irbrc'], Home['.irbrc'] ]
+Link[ Cwd['.musca'], Home['.musca'] ]
+
+Link[ Cwd['.zshrc'], Home['.zshrc'] ]
+Link[ Cwd['.zsh.d'], Home['.zsh.d'] ]
